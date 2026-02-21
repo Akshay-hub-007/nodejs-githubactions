1e4c3b4159de1d1dcdc256d38ef5036fd6e8564e
Akshay-hub-007 <akshaykalangi9@gmail.com>
2026-02-21 10:29:30 +0530

    Add new route for /12 returning a static response

--- src/practice.ts
+++ src/practice.ts
@@ -6,4 +6,7 @@ router.get("/", (req: Request, res: Response) => {
     return res.send("hello from github actio for nodejs")
 })
 
+router.get("/12", (req: Request, res: Response) => {
+    return res.send("2")
+})
 export default router
